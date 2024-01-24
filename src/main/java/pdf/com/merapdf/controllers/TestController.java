9919a0119a3bb39ebcddffed96e92dfb63ceb843
tejagit2015 <103856712+tejagit2015@users.noreply.github.com>
2024-01-24 22:53:17 +0530

    Update TestController.java

--- src/main/java/pdf/com/merapdf/controllers/TestController.java
+++ src/main/java/pdf/com/merapdf/controllers/TestController.java
@@ -9,6 +9,7 @@ public class TestController {
     @GetMapping("/healthCheck")
     public String healthCheck()
     {
+        System.out.println("Health is Super");
         return "<h1><center>I'm up and running</center></h1>";
     }
 }
